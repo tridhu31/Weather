17c7eb1fbf5833100b79267341d18d28a1ac2f54
tridhu31 <89681577+tridhu31@users.noreply.github.com>
2021-08-30 12:59:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function getDetails(place) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById('name').innerHTML = abc.name)
 
 
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
 .then(abc => abc.json())
 .then(abc => document.getElementById('Latitude').innerHTML = abc.coord.lat)
 
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
 .then(abc => abc.json())
 .then(abc => document.getElementById('Longitude').innerHTML = abc.coord.lon)
 
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
 .then(abc => abc.json())
 .then(abc => document.getElementById('Country').innerHTML = abc.sys.country)
 
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
 .then(abc => abc.json())
 .then(abc => document.getElementById('Temperature').innerHTML = parseInt(abc.main.temp-273.15)+"°C")
 
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
 .then(abc => abc.json())
 .then(abc => document.getElementById('speed').innerHTML = abc.wind.speed+"Km/h")
 }
